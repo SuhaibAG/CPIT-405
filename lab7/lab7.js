68197f0f14719de2f3ed19278daa9ea43ed09b85
SuhaibAG <suhaib.ag.49@gmail.com>
2024-10-27 13:51:37 +0300

    Finished lab 7

--- lab7/lab7.js
+++ lab7/lab7.js
@@ -2,8 +2,10 @@ const xhrSearchBtn = document.getElementById("xhrSearch")
 xhrSearchBtn.addEventListener("click", searchUsingXHR)
 
 const fetchSearchBtn = document.getElementById("fetchSearch")
+fetchSearchBtn.addEventListener("click", searchUsingFetchPromises)
 
 const fetchAsyncAwaitBtn = document.getElementById("fetchAsyncAwaitSearch")
+fetchAsyncAwaitBtn.addEventListener("click", searchUsingFetchAsyncAwait)
 
 const searchQuery = document.getElementById("queryInput")
 
@@ -14,38 +16,65 @@ const Access_key = "onV-2LmQ8C2ZHFKGAo-qxS-UPvyIwV1E4FXUEti5rAc"
 function searchUsingXHR(){
     let queryTerm = searchQuery.value.trim();
     const xhr = new XMLHttpRequest();
-
     xhr.open("GET", API_URL + "?query=" + queryTerm)
     xhr.setRequestHeader("Authorization", "Client-ID " + Access_key) 
-    if(xhr.onreadystatechange == 4 && xhr.status == 200){
-        let ResponseText = xhr.responseText;
-        let ResponseObj = JSON.parse(ResponseText);
-        createImages(ResponseObj);
+   
+
+    xhr.onreadystatechange = () => {
+       if(xhr.readyState === 4 && xhr.status === 200){
+            let ResponseText = xhr.responseText;
+            let ResponseObj = JSON.parse(ResponseText);
+            createImages(ResponseObj);
+        }
     }
     xhr.send();
 }
 
 
 function createImages(data){
+    console.log(data)
     const resultsElem = document.getElementById("results");
-    for(let item in data.results){
+    for(let item of data.results){
         let imgElem = document.createElement("img");
         imgElem.src = item.urls.small;
+        imgElem.alt = item.alt_description;
         resultsElem.appendChild(imgElem);
     }
 }
 
-function searchUsingFetchAsyncAwait(){
+async function searchUsingFetchAsyncAwait(){
     let queryTerm = searchQuery.value.trim();
-    let response =  fetch (API_URL, {
+    let response =  await fetch (API_URL + "?query=" + queryTerm, {
         method: "GET",
         headers:{
             "Authorization": "Client-ID " + Access_key
         }
     })
+    
     if(response.ok){
-        const ResponseObj =  response.json
+        const ResponseObj = await response.json();
+        createImages(ResponseObj)
+        console.log(ResponseObj)
     }
 
 
+}
+
+function searchUsingFetchPromises(){
+    let queryTerm = searchQuery.value.trim();
+    fetch (API_URL + "?query=" + queryTerm, {
+        method: "GET",
+        headers:{
+            "Authorization": "Client-ID " + Access_key
+        }
+    })
+    .then(response => {
+        if (!response.ok){
+            throw Error("message")
+        }
+        let ResponseObj =  response.json();
+        console.log(ResponseObj)
+        
+    })
+    
 }
